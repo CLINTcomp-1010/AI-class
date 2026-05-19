ce08137fdaf752d2bb3cf37322ae0b3942fac6a8
Idehen Clinton <idehenclinton291@gmail.com>
2026-05-19 11:22:41 +0100

    Add python and insert file inside change s in string:

---
+++ python/extract.py
@@ -0,0 +1,18 @@
+def extract_characters(name, n):
+    #create a variable to store the extracted characters
+    result_name = ""
+    #index from the argument string
+    index = 0
+
+    while index != n:
+        result_name = result_name + name[index]
+        index += 1
+
+    return result_name
+
+
+
+if __name__ == "__main__":
+    n = int(input("enter n characters to be extracted: "))
+    name = input("enter string:")
+    print(extract_characters(name, n))
